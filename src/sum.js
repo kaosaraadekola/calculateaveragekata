ad094ee0b011f5dfe6e5e7e7979dc2f98acf16a9
Kaosara Adekola <kaosaraidris@hotmail.co.uk>
2024-01-16 11:03:21 +0000

    code working but no unit tests were carried out. did this kata with tomi

--- src/sum.js
+++ src/sum.js
@@ -1,10 +1,27 @@
-/** Adds two given numbers, returning the sum.
- * @param {number} a
- * @param {number} b
- * @returns {number} the sum of a and b
+/** Calculating the average in a given array
+ * @param {array []} 
+ * @returns {number} 
  */
-function sum(a, b) {
-    return a + b;
-}
 
-export { sum };
+let arrayOfNumbers = [10,20,9,8,100]
+
+const calculateAverageNumber = (arrayOfNumbers) => {
+    
+    let sum = 0
+    
+    for (let i = 0; i < arrayOfNumbers.length; i++) {
+
+        sum += arrayOfNumbers[i]
+   
+    }
+
+    const average = sum / arrayOfNumbers.length
+
+    return average 
+
+    }
+
+
+const average = calculateAverageNumber (arrayOfNumbers)
+console.log (average)
+export { calculateAverageNumber };
